43bfd6f1db2ed332d4927693b9972b50d606191c
saniyamemon2702 <saniyamemon7862@gmail.com>
2022-10-24 13:47:47 -0400

    Navbar completed along with styling!

--- myfirstreactapp/src/Components/Navbar.js
+++ myfirstreactapp/src/Components/Navbar.js
@@ -1,5 +1,11 @@
 import  React from "react";
 
 export default function Navbar(){
-    return (<h1>Navbar component</h1>);
+    return (
+        <nav>
+            <img src="./logo192.png" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    );
 }
