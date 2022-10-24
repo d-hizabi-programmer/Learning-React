dfcc6225cd799d4b839c539bffbb4c2511e70dfb
saniyamemon2702 <saniyamemon7862@gmail.com>
2022-10-24 13:26:18 -0400

    committing first time: Components added!

---
+++ myfirstreactapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar'; 
+import Main from './Components/Main';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main /> 
+   </div>
+   );
+}
+
+export default App;
+ 
