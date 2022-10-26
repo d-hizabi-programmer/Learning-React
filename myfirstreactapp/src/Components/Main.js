ac5bd8ca87b491c27d8f98f548aaa0f81481a7b8
saniyamemon2702 <saniyamemon7862@gmail.com>
2022-10-26 11:44:17 -0400

    Main structure ready! styles remaining

--- myfirstreactapp/src/Components/Main.js
+++ myfirstreactapp/src/Components/Main.js
@@ -1,5 +1,17 @@
 import  React from "react";
 
 export default function Main(){
-    return (<h1>Main Component</h1>);
+    return (
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k star on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise app, including mobile app</li>
+
+            </ul>
+        </main>
+    );
 }
