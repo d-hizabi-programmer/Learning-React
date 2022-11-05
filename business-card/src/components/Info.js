e5d4f1bfc358534ec8bface54514ee83f24c95bd
saniyamemon2702 <saniyamemon7862@gmail.com>
2022-11-04 21:09:49 -0400

    Complete: Buttons and Headings design

--- business-card/src/components/Info.js
+++ business-card/src/components/Info.js
@@ -6,8 +6,17 @@ export default function Info(){
     return (
         <div className="info-container">
             <img className="info-image" src={me}  title="Saniya Memon" alt="Saniya Memon" />
+            <div className="info-text">
             <h1>Saniya Memon</h1>
-            <h3>Web Developer</h3>
+            <h3 className="text-warning">Front-end Developer</h3>
+            <div className="info-buttons">
+            <a class="btn btn-warning" href="mailto:saniyamemon7862@gmail.com" role="button"><i class="fa fa-envelope"></i> Mail</a>
+
+            <a class="btn btn-primary" href="https://www.linkedin.com/in/saniyamemon/" role="button"><i class="fa fa-linkedin-square"></i> LinkedIn</a>
+            <a class="btn btn-danger" href="https://github.com/saniyamemon2702" role="button"><i class="fa fa-github" ></i> GitHub</a>
+
+ </div>
+        </div>
         </div>
     );
 }
