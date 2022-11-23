ad1da6653a1ee00c97a4fe0edbc70c9bb2f475ad
saniyamemon2702 <saniyamemon7862@gmail.com>
2022-11-23 14:46:05 -0500

    Created: hero component!

--- airbnb-experience-clone/src/Components/Hero.js
+++ airbnb-experience-clone/src/Components/Hero.js
@@ -4,13 +4,13 @@ import photogrid from "../Images/photo-grid.png";
 export default function Navbar(){
     return (
         <>
-        <div className="hero">
-            <img className="hero--image" src={photogrid} alt="Photo-grid" />
-            <h1 className="hero--heading">Online Experiences</h1> 
-            <p className="hero--text">
+        <section className="hero d-flex flex-column justify-content-center align-items-center">
+            <img className="hero--image img-fluid" src={photogrid} alt="grid" />
+            <h1 className="hero--heading align-self-sm-start mt-3  mx-5 align-self-center">Online Experiences</h1> 
+            <p className="hero--text align-self-sm-start mt-2 align-self-center  mx-5 text-wrap" >
                 Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.
             </p>
-        </div>
+        </section>
         </>
     );
 }
