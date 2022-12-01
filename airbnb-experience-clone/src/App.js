8ca548184ccca03ab1c4a4ca69e6224536b039a1
saniyamemon2702 <saniyamemon7862@gmail.com>
2022-12-01 14:19:01 -0500

    Card design completed!

--- airbnb-experience-clone/src/App.js
+++ airbnb-experience-clone/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
+import Card from './Components/Card';
 
 function App() {
   return (
     <>
        <Navbar />
        <Hero />
+       <Card />
     </>
   
   );
--- airbnb-experience-clone/src/Components/Card.js
+++ airbnb-experience-clone/src/Components/Card.js
@@ -1,14 +1,14 @@
 import React from "react";
-import photogrid from "../Images/photo-grid.png";
+import kz from "../Images/kz.jpg";
 export default function Card() {
     return (
      
             <div class="card" style={{"width": "18rem"}}>
-                <img class="card-img-top" src={photogrid} alt="Card cap" />
+                <img class="card-img-top" src={kz} alt="Card cap" />
                 <div class="card-body">
-                <p class="card-text"><i class="fa-solid fa-star"></i> 5.0 (6)</p> 
+                    <p class="card-text"><i  className="fa fa-star checked"></i> 5.0 (6) &#x2022; USA</p> 
                     <h5 class="card-title">Life  lessons with Katie Zaferes</h5>
-                    <p class="card-text"><span className="font-weight-bold">$136</span>/Person</p>
+                    <p class="card-text"><b>$136</b>/Person</p>
                     
                 </div>
             </div>
